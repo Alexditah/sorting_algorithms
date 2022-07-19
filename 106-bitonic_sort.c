5eeae22932ba54f866a3260f26304b60f19eecfd
Alexditah <99592915+Alexditah@users.noreply.github.com>
2022-07-19 23:46:53 +0100

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,88 +1,91 @@
 #include "sort.h"
 
-/**
-* bitonic_sort - Implements bitonic sort
-* @array: pointer
-* @size: zu size of array
-*/
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_sort(int *array, size_t size);
 
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bitonic_merge - Sort a bitonic sequence inside an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @start: The starting index of the sequence in array to sort.
+ * @seq: The size of the sequence to sort.
+ * @flow: The direction to sort in.
+ */
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow)
+{
+	size_t i, jump = seq / 2;
+
+	if (seq > 1)
+	{
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+			    (flow == DOWN && array[i] < array[i + jump]))
+				swap_ints(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
+}
+
+/**
+ * bitonic_seq - Convert an array of integers into a bitonic sequence.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @start: The starting index of a block of the building bitonic sequence.
+ * @seq: The size of a block of the building bitonic sequence.
+ * @flow: The direction to sort the bitonic sequence block in.
+ */
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
+
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
+}
+
+/**
+ * bitonic_sort - Sort an array of integers in ascending
+ *                order using the bitonic sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap. Only works for
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2).
+ */
 void bitonic_sort(int *array, size_t size)
 {
-	/* guard */
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
-	dohalf(array, 1, size, size);
-}
 
-/**
-* dohalf - Split array
-* @array: pointer
-* @flag: int 0 or 1
-* @size: zu
-* @initsize: full size of array
-*/
-
-void dohalf(int *array, int flag, size_t size, size_t initsize)
-{
-	size_t half = (size / 2);
-
-	if (size < 2)
-	{
-		return;
-	}
-
-	/* Merging operation up or down according to flag */
-	printf("Merging [%lu/%lu] (%s):\n", size, initsize,
-		(flag == 1) ? "UP" : "DOWN");
-	print_array(array, size);
-	/* recursion up */
-	dohalf(array, 1, half, initsize);
-	/* recusrsion down */
-	dohalf((array + half), 0, half, initsize);
-	/* join in recursion */
-	merge(array, flag, size);
-	/* show result */
-	printf("Result [%lu/%lu] (%s):\n", size, initsize,
-		(flag == 1) ? "UP" : "DOWN");
-	print_array(array, size);
-}
-
-/**
-* merge - do the merge
-* @array: pointer
-* @flag: set up or down
-* @size: array max
-*/
-
-void merge(int *array, int flag, size_t size)
-{
-	size_t i, gap;
-	int aux;
-
-	/* split again */
-	size_t half = size / 2;
-
-	if (size < 2)
-	{
-		return;
-	}
-
-	gap = size / 2;
-
-	for (i = 0; i < gap; i++)
-	{
-		if ((array[i] > array[i + gap]) == flag)
-		{
-			/* do the swap */
-			aux = array[i];
-			array[i] = array[i + gap];
-			array[i + gap] = aux;
-		}
-	}
-
-	/* recursion merge */
-	merge(array, flag, half);
-	merge((array + half), flag, half);
+	bitonic_seq(array, size, 0, size, UP);
 }
